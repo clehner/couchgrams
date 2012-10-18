dac9910600f99fe04d8728e70365d36f1bc02ed2
Charles Lehner <celehner1@gmail.com>
2012-10-17 20:25:17 -0400

    Allow updating old text with _update/add_text

--- updates/add_text.js
+++ updates/add_text.js
@@ -1,14 +1,7 @@
 function(doc, req) {
-	// only allow putting new text, not updating old text
-	if (doc) return [null, {
-		code: 409,
-		body: "conflict"
-	}];
-
-	var text = req.body;
-	var doc = {
-		_id: (new Date()/1000).toString(),
-		text: text
+	if (!doc) doc = {
+		_id: req.id || (new Date()/1000).toString()
 	};
+	doc.text = req.body;
 	return [doc, "ok"];
 }
