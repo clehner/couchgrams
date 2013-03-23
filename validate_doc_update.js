e1bb2a35d8d2d4a27c9afe83d61ee2915cb1bd5b
Charles Lehner <celehner1@gmail.com>
2013-03-23 02:36:28 -0400

    Fix validate function typo

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,5 +1,5 @@
 function (doc, oldDoc, userCtx) {
-	if (userCtx.roles.indexOf('couchgrams' == -1) &&
+	if (userCtx.roles.indexOf('couchgrams') == -1 &&
 		userCtx.roles.indexOf('_admin') == -1) {
 		throw {unauthorized: 'Not permitted'};
 	}
